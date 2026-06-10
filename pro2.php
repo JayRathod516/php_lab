a09aa3ea3d72d102041cc9f26ad7b3c6a3d67f27
JayRathod516 <jay.rathod127631@marwadiuniversity.ac.in>
2026-06-10 13:06:48 +0530

    Add files via upload

---
+++ pro2.php
@@ -0,0 +1,12 @@
+<?php
+
+$a = 10;
+$b = 25;
+
+if ($a > $b) {
+    echo "Maximum Number = " . $a . "<br>";
+    echo "Minimum Number = " . $b;
+} else {
+    echo "Maximum Number = " . $b . "<br>";
+    echo "Minimum Number = " . $a;
+}
